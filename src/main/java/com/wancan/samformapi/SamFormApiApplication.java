39e6fc65a31773b25dce33adb90118be6199cc9a
caneel7 <caneelmacwan98@gmail.com>
2024-03-30 18:26:40 +0530

    init

---
+++ src/main/java/com/wancan/samformapi/SamFormApiApplication.java
@@ -0,0 +1,13 @@
+package com.wancan.samformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamFormApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamFormApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wancan/samformapi/SamFormApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wancan.samformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamFormApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
